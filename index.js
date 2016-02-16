8e17695c106007db6b597f10ceeaeae2d9569cdd
Amulya Kumar Sahoo <amulya349@gmail.com>
2016-02-16 16:19:37 +0530

    Added code to display in browser
    I was unable to view the whole result as json in my shell. This code
    views the json in browser, which is more interactive.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var express = require('express');
+var app = express();
+var port 	= process.env.PORT || 8080;
+var open = require("open");
+
 var secrets = require('./secrets.json')
 		, credentials = secrets["ibm-personality-insights"].credentials
 		, watson = require('watson-developer-cloud')
@@ -8,12 +13,27 @@ var secrets = require('./secrets.json')
 		})
 		, speeches = require('./speeches.json');
 
+
 personality_insights.profile({text: speeches["winston-churchill"]["their-finest-hour"]},
 	function(err, profile) {
 		if(err) {
 			console.log(err);
 		} else {
 			console.log(profile);
+			trigger(profile);
 		}
 	}
 );
+
+function trigger(profile){
+	app.listen(port);
+
+	// console.log("Open http://localhost:8080 in the browser to view the results clearly !!");
+	app.get('/', function(req, res){
+		res.json(profile);
+	})
+
+	//Launch the browser
+	open("http://localhost:8080");
+
+}
